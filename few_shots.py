e2a7a4f20ec0e64c008d6b9a0a46fae0b630f642
Nirbhay Sedha <95143253+Nirbhaysedha@users.noreply.github.com>
2023-11-23 16:16:37 +0530

    updated

--- few_shots.py
+++ few_shots.py
@@ -2,31 +2,37 @@ kpmg_questions = [
     {
         'Question': "How many employees work in the Finance department?",
         'SQLQuery': "SELECT COUNT(*) FROM Employees WHERE Department = 'Finance'",
+        'SQLResult': "Result of the SQL query",
         'Answer': "3"
     },
     {
         'Question': "What is the average salary of employees hired in 2023?",
         'SQLQuery': "SELECT AVG(Salary) FROM Employees WHERE YEAR(HireDate) = 2023",
+        'SQLResult': "Result of the SQL query",
         'Answer': "67600.00"
     },
     {
         'Question': "List employees who have managers and their manager's names.",
         'SQLQuery': "SELECT e.FirstName, e.LastName, m.FirstName AS Manager_FirstName, m.LastName AS Manager_LastName FROM Employees e LEFT JOIN Employees m ON e.ManagerID = m.EmployeeID WHERE e.ManagerID IS NOT NULL",
+        'SQLResult': "Result of the SQL query",
         'Answer': "Sample result set with employee names and their manager names"
     },
     {
         'Question': "How many employees were hired before January 1, 2022?",
         'SQLQuery': "SELECT COUNT(*) FROM Employees WHERE HireDate < '2022-01-01'",
+        'SQLResult': "Result of the SQL query",
         'Answer': "5"
     },
     {
         'Question': "Count the number of employees in each department.",
         'SQLQuery': "SELECT Department, COUNT(*) AS EmployeeCount FROM Employees GROUP BY Department",
+        'SQLResult': "Result of the SQL query",
         'Answer': "Sample result set with department names and employee counts"
     },
     {
         'Question': "Retrieve the email addresses and phone numbers of all employees.",
         'SQLQuery': "SELECT FirstName, LastName, Email, PhoneNumber FROM Employees",
+        'SQLResult': "Result of the SQL query",
         'Answer': "Sample result set with employee names, emails, and phone numbers"
     }
 ]
